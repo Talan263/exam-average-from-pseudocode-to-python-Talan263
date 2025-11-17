42b070abc902a4fb8627ab25af0ff5960febe474
IS250 Student <changeme@example.com>
2025-11-17 13:11:46 -0600

    added pseudocode

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 """
 Full Name: Talan Wessman
-Class-Section:
-Assignment Title:
-Submission Date:
+Class-Section: IS 250-01
+Assignment Title: Swtup Activity
+Submission Date: November 17th
 """
 """
-Write your pseudocode here.
+#define your function as calculate_average()
+#prompt the user to enter the scores
+#add the three scores together
+#divide the total score by the number of scores entered
+#display the individual scores and the average
 Do not write any Python code in this section.
 Your pseudocode should describe your overall approach in your own words.
 """
