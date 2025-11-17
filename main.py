11d469b40689eef7b7fc4955ac1ec65a6dd6a565
IS250 Student <changeme@example.com>
2025-11-17 13:18:58 -0600

    added code

--- main.py
+++ main.py
@@ -15,7 +15,19 @@ Your pseudocode should describe your overall approach in your own words.
 """
 
 # Your Python code begins below this line.
-# Every line you write must have a comment directly above it.
+#define the function
+def calculate_average():
+  #prompt user to enter score 1
+  score1=int(input("Enter the first score: "))
+  #prompt the user to enter score 2
+  score2=int(input("Enter the second score: "))
+  #prompt the user to enter score 3
+  score3=int(input("Enter the third score: "))
+  #calculate the average of the 3 scores
+  average= (score1+score2+score3)/3
+  #print the average of the 3 scores
+  print("First score:",score1,"\n""Second score:",score2,"\n","Third Score:",score3,"\n","The average score is:",average,)
+
 
 # Call your function when your program is ready
 calculate_average()   
